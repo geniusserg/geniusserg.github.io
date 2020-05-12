5c389ee196a556dd8d4f78c1fd8b7fef5d346f3b
geniusserg <50704370+geniusserg@users.noreply.github.com>
2020-05-12 16:17:00 +0300

    Update test.py

--- tests/test.py
+++ tests/test.py
@@ -15,7 +15,7 @@ class TestTemplate(unittest.TestCase):
         self.driver.quit()
 
     def test_case_1(self):
-        self.driver.get('https://localhost:8080')
+        self.driver.get('http://localhost:8080')
         title = self.driver.title
         self.assertEqual(title, 'CarConfigurator')
 
