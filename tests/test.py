529a566452cb2d68987c60ddb06355cb37f1279e
geniusserg <50704370+geniusserg@users.noreply.github.com>
2020-05-12 16:47:32 +0300

    Update test.py

--- tests/test.py
+++ tests/test.py
@@ -8,7 +8,7 @@ class TestTemplate(unittest.TestCase):
         chrome_options.add_argument('--no-sandbox')
         chrome_options.add_argument('--headless')
         chrome_options.add_argument('--disable-gpu')
-        self.driver = webdriver.Chrome(chrome_options=chrome_options)
+        self.driver = webdriver.Chrome(executable_path="/opt/chrome_driver/chromedriver")
         self.driver.implicitly_wait(10)
 
     def tearDown(self):
