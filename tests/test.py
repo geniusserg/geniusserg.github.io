d183b192988f1ff8db0f27a1c1c6b32a8e322378
geniusserg <50704370+geniusserg@users.noreply.github.com>
2020-05-12 17:01:43 +0300

    Update test.py

--- tests/test.py
+++ tests/test.py
@@ -8,7 +8,7 @@ class TestTemplate(unittest.TestCase):
         chrome_options.add_argument('--no-sandbox')
         chrome_options.add_argument('--headless')
         chrome_options.add_argument('--disable-gpu')
-        chrome_options.add_arguments('--disable-dev-shm-usage');
+        chrome_options.add_argument('--disable-dev-shm-usage')
         self.driver = webdriver.Chrome(executable_path="/opt/chrome_driver/chromedriver", chrome_options=chrome_options)
         self.driver.implicitly_wait(10)
 
